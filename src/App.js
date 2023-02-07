e89c4e114864b6233f33a7d9f87996c3c77d0d51
Jeff Trojan <j.dauda@alustudent.com>
2023-02-07 02:57:45 +0200

    Nav links and media query for them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "styled-components";
-import { lightTheme } from 'styles/theme'
+import { GlobalStyle } from "./GlobalStyle";
+import { lightTheme } from './styles/theme'
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       {" "}
+      <GlobalStyle />
 
         <h1> Fitness site</h1>
     </ThemeProvider>
---
+++ src/components/NavBar/NavBarStyle.js
@@ -0,0 +1,61 @@
+import styled  from "styled-components";
+export const NavContainer = styled.div`
+
+    display: flex;
+    justify-content:space-between;
+    align-items: center;
+
+`;
+
+export const TitleLink = styled.link`
+
+    color: ${props => props.theme.TextColor};
+    font-size: 2rem;
+    font-weight: bold,
+
+`
+export const NavList = styled.ul`
+    display: flex;
+    align-items: center;
+    column-gap: 2rem;
+    transition: .5s;
+
+    @media (max-wigth:767px) {
+        flex-direction: column;
+        row-gap: 5rem;
+        padding: 5rem;
+        background-color: ${props => props.theme.ContainerColor};
+        position: fixed;
+        width: 350px;
+        min-height: 100vh;
+        top: 55px;
+        box-shadow:  0px 5px rgba(255, 255, 255, 0.47);
+        right: 0;
+    }
+
+
+`;
+
+export const NavItem = styled.li ``;
+
+export const NavLink = styled.link`
+    font-size: 1.5rem;
+    color: ${props => props.about.theme.TextColor};
+    transition: .4s;
+    &:hover {
+       color: ${props => props.about.theme.PrimaryColor};
+
+    }
+    @media ${device.tablet}{
+        font-size: 1.6rem;
+    }
+
+
+    @media ${device.laptop}{
+        font-size: 2rem;
+    }
+
+`;
+
+
+
