23f7f6df067560a113581398ffcfd0e8e6ddfd73
Jeff Trojan <j.dauda@alustudent.com>
2023-02-07 03:32:19 +0200

    Styled NavBar | incl hamburger and NavIcon

--- src/components/NavBar/NavBarStyle.js
+++ src/components/NavBar/NavBarStyle.js
@@ -1,4 +1,5 @@
 import styled  from "styled-components";
+import { device } from "../../styles/theme";
 export const NavContainer = styled.div`
 
     display: flex;
@@ -57,5 +58,23 @@ export const NavLink = styled.link`
 
 `;
 
+export const NavIcon = styled.div`
+    svg {
+        font-size: 2.2rem;
+        cursor: pointer;
+        fill: ${props => props.theme.TextColor};
 
+    }
+`
 
+export const Hamburger = styled.div`
+    font-size: 2rem;
+    cursor: pointer;
+    color: ${props => props.theme.TextColor};
+
+    @media ${device.tablet} {
+        display: none;
+
+    }
+
+`;
